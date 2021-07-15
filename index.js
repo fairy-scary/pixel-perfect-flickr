0b98ad8f15d4cc05b3416d4e4fbd7b84804b9437
Autumn Wiggins <heartofgold@Autumns-MBP.attlocal.net>
2021-07-15 12:05:02 -0500

    added nav button and search details

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+// DOM Manipulation with Javascript(Week 9): Randomizes background image every 2 seconds
 
-// randomizes background image every 2 seconds
-changeBackground = () => {
+//---code starts--->
+
+// changeBackground = () => {
     
-    const images = [ 'url("./assets/fam.jpg")',
-        'url("./assets/desk.jpeg")',
-        'url("./assets/grandbudapest.jpg")',
-        'url("./assets/moonrise2.jpg")',
-        'url("./assets/lifeaquatic.jpg")'
-    ];
+//     const images = [ 'url("./assets/fam.jpg")',
+//         'url("./assets/desk.jpeg")',
+//         'url("./assets/grandbudapest.jpg")',
+//         'url("./assets/moonrise2.jpg")',
+//         'url("./assets/lifeaquatic.jpg")'
+//     ];
 
-    const contentDiv = document.getElementsByClassName("content")[0];
-    const bg = images[Math.floor(Math.random() * images.length)];
-    //console.log(contentDiv.style) //*see the HTML DOM Style Object(under "style")
+//     const contentDiv = document.getElementsByClassName("content")[0];
+//     const bg = images[Math.floor(Math.random() * images.length)];
+//     //console.log(contentDiv.style) //*see the HTML DOM Style Object(under "style");
     
-    contentDiv.style.backgroundImage = bg
+//     contentDiv.style.backgroundImage = bg;
 
-}
+// }
 
-setInterval(changeBackground,2000)
+// setInterval(changeBackground,3000);
+
+//<---code ends---
 
